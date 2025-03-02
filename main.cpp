47544964da429063e4c14deed912eded3655a750
Purple-Rose <mirzeinalyehsan@gmail.com>
2025-03-03 00:18:41 +0330

    hello oop222

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout<<"Hello Word";
+    std::cout<<"Hello Worddddddddddd";
     return 0;
 }
---
+++ student.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Purple-Rose on 3/3/2025.
+//
+
+#include "student.h"
